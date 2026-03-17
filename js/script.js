8eec7573ac95cb58a43f04f8d1ed4599aa4c28b5
moabbas7004-create <mo.abbas7004@gmail.com>
2026-03-17 12:48:01 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,13 +2,9 @@ var sf = new Snowflakes({
     color: "#ffd700",
     minSize: 20
 });
-var url_string = window.location.href; //window.location.href
-var url = new URL(url_string);
-var c = url.searchParams.get("name");
-console.log(c);
-if (c != null) {
-    document.getElementById("name").innerHTML = c;
-    document.getElementById("nae").innerHTML = c;
+{
+    document.getElementById("name").innerHTML = "Abbas";
+    document.getElementById("nae").innerHTML = "Abbas";
 }
 $(".main").fadeOut(1);
 $('#play').click(function () {
